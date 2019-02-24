b7033b26e26dfc6d85b5c5173650cc07b7fb864f
unknown <mmkhitar13@gail>
2019-02-24 12:15:38 -0800

    mobile

---
+++ project.js
@@ -0,0 +1,9 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const top1 = document.querySelector(".top");
+const headerMenu = document.querySelector(".header-menu");
+
+hamburgerMenu.addEventListener("click",function () {
+    top1.style.display = "none";
+
+
+});
